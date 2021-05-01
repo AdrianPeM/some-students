ceb13e7a416f9e67f0918b85cfbacbfd7eacbc33
Alfonso <loredo.m.98@gmail.com>
2021-04-30 17:00:03 -0700

    Agregar nuevo css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'SOME - Una intervención educativa') }}</title>
+        <title>{{ config('app.name') }} - {{ config('app.subtitle') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -15,13 +15,14 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/views/navigation/navigation.css') }}">
         <link rel="stylesheet" href="{{ asset('css/controls/controls.css') }}">
         {{ $styles ?? ''}}
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
         {{ $scripts ?? ''}}
-        
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
